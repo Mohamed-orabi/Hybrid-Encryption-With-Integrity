79753e70cba8a7e58547a21db8bc40e23366bf20
Mohamed <Mohamed@DESKTOP-PKB020P>
2022-10-01 23:29:24 +0200

    Add project files.

---
+++ HybridEncryption.cs
@@ -0,0 +1,59 @@
+﻿using System.Security.Cryptography;
+
+namespace CryptographyInDotNet
+{
+    public class HybridEncryption
+    {
+        private readonly AesEncryption _aes = new AesEncryption();
+
+        public EncryptedPacket EncryptData(byte[] original, RSAWithRSAParameterKey rsaParams)
+        {
+            var sessionKey = _aes.GenerateRandomNumber(32);
+            var encryptedPacket = new EncryptedPacket { Iv = _aes.GenerateRandomNumber(16) };
+
+            // Encrypt data with AES and AES Key with RSA
+            encryptedPacket.EncryptedData = _aes.Encrypt(original, sessionKey, encryptedPacket.Iv);
+            encryptedPacket.EncryptedSessionKey = rsaParams.EncryptData(sessionKey);
+
+            using (var hmac = new HMACSHA256(sessionKey))
+            {
+                encryptedPacket.Hmac = hmac.ComputeHash(encryptedPacket.EncryptedData);
+            }
+
+            return encryptedPacket;
+        }
+
+        public byte[] DecryptData(EncryptedPacket encryptedPacket, RSAWithRSAParameterKey rsaParams)
+        {
+            // Decrypt AES Key with RSA and then decrypt data with AES.
+            var decryptedSessionKey = rsaParams.DecryptData(encryptedPacket.EncryptedSessionKey);
+
+            using (var hmac = new HMACSHA256(decryptedSessionKey))
+            {
+                var hmacToCheck = hmac.ComputeHash(encryptedPacket.EncryptedData);
+
+                if (!Compare(encryptedPacket.Hmac, hmacToCheck))
+                {
+                    throw new CryptographicException("HMAC for decryption does not match encrypted packet.");
+                }
+            }
+
+            var decryptedData = _aes.Decrypt(encryptedPacket.EncryptedData, decryptedSessionKey, encryptedPacket.Iv);
+
+            return decryptedData;
+        }
+
+        private static bool Compare(byte[] array1, byte[] array2)
+        {
+            var result = array1.Length == array2.Length;
+
+            for (var i = 0; i < array1.Length && i < array2.Length; ++i)
+            {
+                result &= array1[i] == array2[i];
+            }
+
+            return result;
+        }
+
+    }
+}
